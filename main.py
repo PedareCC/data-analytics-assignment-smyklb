98e1dc9567c21f32f701723a70277aa1ec422446
smyklb <162660374+smyklb@users.noreply.github.com>
2024-08-09 02:43:20 +0000

    Total tourists for 2019-2024

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import numpy as np
 
 #Read the CSV file into a DataFrame
-dataframe = pd.read_csv('Tourism.csv')
+df= pd.read_csv('Tourism.csv')
 
-#Print the DataFrame. You can delete this later. 
-print(dataframe)
+# total tourists for each year
+total_2019 = df["Trips Ending March 2019"].sum()
 
+total_2024 = df["Trips Ending March 2024"].sum()
+
+# print the total number of tourists
+print(f"Total 2019 tourists = {total_2019}\n Total 2024 tourists = {total_2024}\n")
+
+# print data type
+print(df.dtypes)
